9b0a4d14e02f2ba67acc2d0ce5df37a4cb2d67ed
Søren Seeberg <31856851+SorenSeeberg@users.noreply.github.com>
2018-07-13 23:22:35 +0200

    Selection
    Added beginning of multi-selection to grid

--- src/components/PreviewGrid/index.js
+++ src/components/PreviewGrid/index.js
@@ -1,52 +1,60 @@
 import React from "react";
 import "./style.css";
 
-const images = [
-  { name: "Image01.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image02.png", x: 4096, y: 4096, depth: 24, channels: "RBG" },
-  { name: "Image03.png", x: 4096, y: 4096, depth: 8, channels: "Palette" },
-  { name: "Image04.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image05.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image06.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image07.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image08.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image09.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image10.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image11.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image12.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image13.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image14.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image15.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image16.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image17.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image18.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image19.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image20.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image21.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
-  { name: "Image22.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" }
-];
+class PreviewContainer extends React.Component {
+  render() {
+    const { data, selected, onClick } = this.props;
 
-const PreviewContainer = ({ data }) => (
-  <div className="preview-grid-container">
-    <div className="preview-grid-image-overlay">
-      <div className="preview-grid-image-infobox">{data.name}</div>
-      <div className="preview-grid-image-infobox">{`[ ${data.x} x ${data.y} @ ${
-        data.depth
-      } bit ] ${data.channels}`}</div>
-    </div>
-    <img
-      className="preview-grid-image"
-      src="https://uploads.codesandbox.io/uploads/user/a6d53d72-2d38-4a85-a5b1-d13b3f47fda5/gItd-floor.jpg"
-      alt={data.name}
-    />
-  </div>
-);
+    return (
+      <div className={`preview-grid-container ${selected && "selected"}`}>
+        <div className="preview-grid-image-overlay" onClick={onClick}>
+          <div className="preview-grid-image-infobox">{data.name}</div>
+          <div className="preview-grid-image-infobox">{`[ ${data.x} x ${
+            data.y
+          } @ ${data.depth} bit ] ${data.channels}`}</div>
+        </div>
+        <img
+          className="preview-grid-image"
+          src="https://uploads.codesandbox.io/uploads/user/a6d53d72-2d38-4a85-a5b1-d13b3f47fda5/gItd-floor.jpg"
+          alt={data.name}
+        />
+      </div>
+    );
+  }
+}
 
 export default class PreviewGrid extends React.Component {
+  state = { selection: [] };
+
+  componentDidMount = () => {
+    this.setState({
+      selection: Array.from(
+        { length: this.props.metaData.length },
+        (x, i) => false
+      )
+    });
+  };
+
+  handleClick = index => {
+    console.log(index);
+    const modifiedArray = this.state.selection;
+    modifiedArray[index] = !modifiedArray[index];
+    this.setState({ selection: modifiedArray });
+  };
+
   render() {
+    const { metaData } = this.props;
+
     return (
       <div className="preview-grid">
-        {images.map(data => <PreviewContainer data={data} />)}
+        {metaData.map((metaItem, i) => (
+          <PreviewContainer
+            data={metaItem}
+            selected={this.state.selection[i]}
+            key={i}
+            onClick={() => this.handleClick(i)}
+          />
+        ))}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,99 @@ import ReactDOM from "react-dom";
 import PreviewGrid from "./components/PreviewGrid";
 import "./styles.css";
 
+const metaData = [
+  {
+    name: "wall_old_stone_001.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "wall", "tiling"]
+  },
+  {
+    name: "wall_old_stone_002.png",
+    x: 4096,
+    y: 4096,
+    depth: 24,
+    channels: "RBG",
+    tags: ["texture", "wall"]
+  },
+  {
+    name: "wall_old_stone_003.png",
+    x: 4096,
+    y: 4096,
+    depth: 8,
+    channels: "Palette",
+    tags: ["texture", "wall"]
+  },
+  {
+    name: "floor_broken_tiles_001.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles", "weathered"]
+  },
+  {
+    name: "floor_broken_tiles_002.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles", "weathered"]
+  },
+  {
+    name: "floor_broken_tiles_003.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles", "cracked"]
+  },
+  {
+    name: "floor_tiles_001.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles"]
+  },
+  {
+    name: "floor_tiles_002.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles"]
+  },
+  {
+    name: "floor_tiles_003.png",
+    x: 4096,
+    y: 4096,
+    depth: 32,
+    channels: "RBGA",
+    tags: ["texture", "floor", "tiles"]
+  },
+  { name: "Image10.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image11.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image12.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image13.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image14.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image15.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image16.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image17.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image18.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image19.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image20.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image21.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" },
+  { name: "Image22.png", x: 4096, y: 4096, depth: 32, channels: "RBGA" }
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Preview Grid</h1>
-      <PreviewGrid />
+      <PreviewGrid metaData={metaData} />
     </div>
   );
 }
